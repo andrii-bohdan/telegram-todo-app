ee2a599f3d904e7239fbb5ddcaba7f04195b0a4f
Andriy Bohdan <andrewdevs@protonmail.com>
2022-08-26 18:42:14 +0200

    feat(prisma): initialize prisma

---
+++ prisma/migrations/20220826152910_todo/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `TodoApp` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `author` VARCHAR(191) NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
