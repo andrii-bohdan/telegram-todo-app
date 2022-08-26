2b292485c5f66f98d10beaae7291bc29ce091a89
Andriy Bohdan <andrewdevs@protonmail.com>
2022-08-26 23:23:01 +0200

    fix():  types

--- src/commons/interface/context.interface.ts
+++ src/commons/interface/context.interface.ts
@@ -1,26 +1,6 @@
 import { Context as ContextTelegraf } from 'telegraf';
 export interface Context extends ContextTelegraf {
   session: {
-    type?: 'done' | 'edit' | 'delete' | 'create' | 'list';
+    type?: 'done' | 'edit' | 'delete' | 'create' | 'delete-all';
   };
-}
-
-export interface List {
-  id: number;
-  description: string;
-  author?: string;
-  isCompleted?: boolean;
-  date: string;
-}
-
-export interface OptionList extends List {
-  title?: string;
-}
-
-export enum ContexType {
-  DONE = 'done',
-  EDIT = 'edit',
-  DELETE = 'delete',
-  CREATE = 'create',
-  LIST = 'list',
 }
