0149ae28f445740a3a82bb56d59147305daff48c
Andriy Bohdan <andrewdevs@protonmail.com>
2022-09-08 12:03:21 +0200

    fix(isNumber): error message

--- src/commons/utils/isNumber.utils.ts
+++ src/commons/utils/isNumber.utils.ts
@@ -1,7 +1,7 @@
 export const isNumber = (value: any, convertToNumber: boolean = true) => {
   const pattern: RegExp = /^\d+\.?\d*$/;
   if (!pattern.test(value)) {
-    throw new Error('Please type number to delete todo');
+    throw new Error('The value must have a characters number!');
   }
   return convertToNumber ? Number(value) : value;
 };
